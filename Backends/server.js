26e69f5ae3dbed831db229945fe69b67ecb4e5ff
pradeep <www.avit.ac.unik.inc23@gmail.com>
2026-05-18 11:01:51 +0530

    vercel.json

--- Backends/server.js
+++ Backends/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-
 import dotenv from "dotenv";
 import cors from "cors";
-
 import contactRoutes from "./routes/contactRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
